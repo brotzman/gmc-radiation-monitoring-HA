0c67c48f48ca31d9128ccf5b5e910f78fca1a435
brotzman <brotzman@users.noreply.github.com>
2026-07-26 21:36:29 +0200

    Add files via upload

--- gmc_radiation_monitor/rootfs/usr/local/lib/gmc_bridge/version.py
+++ gmc_radiation_monitor/rootfs/usr/local/lib/gmc_bridge/version.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-APP_VERSION = "9.4.0"
+APP_VERSION = "10.0.0"
 USER_AGENT = f"GMC-Radiation-Monitor/{APP_VERSION}"
 REPORT_SERVER_VERSION = f"GMCReportServer/{APP_VERSION}"
